d53bbd192bedb3bae08ed1d949e09135c08c6bbc
Hikaru Iwasa <oyogupenguin@gmail.com>
2026-07-04 22:45:03 +0900

    rm:old migrated

--- old_dotfiles/home_dir/.config/fish/config.fish
+++
@@ -1,7 +0,0 @@
-### MANAGED BY RANCHER DESKTOP START (DO NOT EDIT)
-set --export --prepend PATH "/Users/hikaru/.rd/bin"
-### MANAGED BY RANCHER DESKTOP END (DO NOT EDIT)
-
-# Added by LM Studio CLI (lms)
-set -gx PATH $PATH /Users/hikaru/.lmstudio/bin
-# End of LM Studio CLI section
