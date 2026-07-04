aa29939a0bf864a15b212598230eacfa9cf37caa
Hikaru Iwasa <oyogupenguin@gmail.com>
2026-07-04 21:49:54 +0900

    install cli tools

--- nix/home.nix
+++ nix/home.nix
@@ -17,23 +17,26 @@
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
-  home.packages = [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
-
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
+  home.packages = with pkgs; [
+    cocoapods
+    fzf
+    gcc
+    gh
+    ghq
+    git
+    gitui
+    go-task
+    jq
+    mise
+    neovim
+    nixfmt
+    nodejs
+    postgresql
+    python3
+    sheldon
+    sqlite
+    tmux
+    tree
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
@@ -70,6 +73,10 @@
   home.sessionVariables = {
     # EDITOR = "emacs";
   };
+
+  programs.starship.enable = true;
+
+  programs.tmux.enable = true;
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
