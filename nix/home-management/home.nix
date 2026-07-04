d8e7374f3bb5c470bfa6a835a71c979fd44ce1c9
Hikaru Iwasa <oyogupenguin@gmail.com>
2026-07-04 22:36:32 +0900

    zshrc and tmux

--- nix/home-management/home.nix
+++ nix/home-management/home.nix
@@ -35,48 +35,23 @@
     python3
     sheldon
     sqlite
-    tmux
     tree
   ];
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
-  home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
+  home.file.".config/sheldon" = {
+    source = ../../config/sheldon;
+    recursive = true;
   };
 
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/hikaru/etc/profile.d/hm-session-vars.sh
-  #
-  home.sessionVariables = {
-    # EDITOR = "emacs";
+  home.file.".zshrc" = {
+    source = ../../config/zsh/.zshrc;
   };
+
+  imports = [
+    ./tmux.nix
+  ];
 
   programs.starship.enable = true;
-
-  programs.tmux.enable = true;
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
